752fbbeac904846d89e25554c62880551fee19f8
Yuya Matsushima <yuyam@fillin-inc.com>
2016-11-01 00:57:16 +0900

    拡張機能を設定
    middleman 拡張機能導入

--- Gemfile
+++ Gemfile
@@ -1,14 +1,16 @@
-# If you do not have OpenSSL installed, update
-# the following line to use 'http://' instead
 source 'https://rubygems.org'
 
-gem 'slim', '>= 3.0'
 
-# For faster file watcher updates on Windows:
 gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
-
-# windows does not come with time zone data
 gem 'tzinfo-data', platforms: [:mswin, :mingw]
 
-# Middleman Gems
 gem 'middleman', '>= 4.0.0'
+gem 'middleman-blog'
+gem 'middleman-syntax'
+gem 'middleman-autoprefixer'
+gem "middleman-generator-tag"
+gem 'middleman-s3_sync'
+gem 'slim', '>= 3.0'
+gem 'redcarpet'
+gem 'builder'
+gem 'rake'
--- config.rb
+++ config.rb
@@ -1,21 +1,27 @@
 require 'slim'
+require 'redcarpet'
+require 'builder'
 
-###
-# Page options, layouts, aliases and proxies
-###
+Time.zone = 'Tokyo'
 
-# Per-page layout changes:
-#
-# With no layout
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# Build-specific configuration
-configure :build do
-  # Minify CSS on build
-  # activate :minify_css
+activate :directory_indexes
+activate :syntax
+activate :autoprefixer
+activate :generator_tag
+activate :gzip, exts: %w(.css .js .html .xml)
 
-  # Minify Javascript on build
-  # activate :minify_javascript
+set :markdown_engine, :redcarpet
+set :markdown, fenced_code_blocks: true, smartypants: true, tables: true, autolink: true
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
 end
+
+#activate :s3_sync do |s3|
+#  s3.region = 'ap-northeast-1'
+#end
