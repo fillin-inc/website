cd335be97a215f7e5ad6a0b38e597a29bb57dca4
Yuya Matsushima <yuyam@fillin-inc.com>
2016-11-10 03:47:42 +0900

    robots.txt の出力方法を変更

--- config.rb
+++ config.rb
@@ -39,14 +39,6 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
   activate :asset_hash
-
-  activate :robots, rules: [
-    {
-      user_agent: '*',
-      disallow: %w(/inquiry/thanks/ /inquiry/thanks/index.html)
-    }
-  ],
-  sitemap: 'https://www.fillin-inc.com/sitemap/xml'
 end
 
 activate :s3_sync do |s3|
