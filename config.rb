6536dd939d0176ff4442299369b1bc62eef976de
Yuya Matsushima <yuyam@fillin-inc.com>
2020-09-21 11:16:17 +0900

    Add footer

--- config.rb
+++ config.rb
@@ -38,7 +38,7 @@ activate :generator_tag
 activate :vcs_time
 activate :gzip
 activate :asset_hash do |f|
-  f.ignore = [/images\/ogp\/\S+\.(png|jpg|gif)/]
+  f.ignore = [/images\/ogp\/\S+\.(png|jpg|gif)/, /images\/common\/footer_inquiry.png/]
 end
 
 set :markdown_engine, :redcarpet
